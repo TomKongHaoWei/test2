d77c2c48ded93d77c596a573d9753c28e47b6f3e
TomKongHaoWei <583039071@qq.com>
2022-03-26 23:21:07 +0800

    1

--- api/user.js
+++ api/user.js
@@ -1,19 +1,22 @@
-// export default function handler(req, res) {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'application/json');
-//   res.json({ name: 'John Doe' });
-// }
-
-export default function getUserInfo (req, res) {
-  console.log('参数', req);
-  const { sex } = req.query
-  
-  const _sex = sex ? '男' : '女'
-  
+export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ 
-    code: req,
-    '性别': _sex
+    name: 'John Doe',
+    req
   });
 }
+
+// export default function getUserInfo (req, res) {
+//   console.log('参数', req);
+//   const { sex } = req.query
+  
+//   const _sex = sex ? '男' : '女'
+  
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'application/json');
+//   res.json({ 
+//     code: req,
+//     '性别': _sex
+//   });
+// }
