5b6a64b98d4c67295d7c8ea2bbd45efcdc3ac25c
TomKongHaoWei <583039071@qq.com>
2022-03-26 23:51:45 +0800

    1

--- api/user.js
+++ api/user.js
@@ -1,7 +1,13 @@
-const handler = (req, res) => {
+const getUserInfo = (req, res) => {
+  const { name = 'World', sex } = req.query
+  const _sex = sex ? '男' : '女'
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ name: 'John Doe' });
+  res.json({ 
+    name: name,
+    sex: _sex
+  });
 }
 
-export default handler
+export default getUserInfo
--- api/user2.js
+++ api/user2.js
@@ -1,9 +1,9 @@
 module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
   const { name = 'World', sex } = req.query
   const _sex = sex ? '男' : '女'
 
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
   res.json({ 
     name: name,
     sex: _sex
