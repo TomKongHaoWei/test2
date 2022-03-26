307d734956e82c04f759d6e1010e6b77a1f0dace
TomKongHaoWei <583039071@qq.com>
2022-03-26 23:05:50 +0800

    qqqq

---
+++ api/qqq.js
@@ -0,0 +1,23 @@
+export const getUserInfo = (req, res) => {
+  console.log('参数', req);
+  const { sex } = req.query
+  
+  const _sex = sex ? '男' : '女'
+  
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ 
+    code: req,
+    '性别': _sex
+  });
+}
+
+export const setUserInfo = (req, res) => {
+  console.log('参数', req);
+  
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ 
+    '设置': req
+  });
+}
